c6fe1927329b348e0fe88b80033be2a4dd2ae926
Iris Quintero <qupi_44@hotmail.com>
2021-10-13 18:46:25 -0500

    fetch

--- src/Body/Body.js
+++ src/Body/Body.js
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useEffect, useState} from "react";
 import "./Body.css";
 
-function Body(){
-return(
-    <div className = "Body_Big_Container">
-        <div className = "Body_Big_Container_box">caja1</div>
-        <div className = "Body_Big_Container_box">caja2</div>
-        <div className = "Body_Big_Container_box">caja3</div>
-        <div className = "Body_Big_Container_box">caja4</div>
-    </div>
-)
+function Body() {
+  const [ticketsList, setTicketsList] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/tickets/getTickets")
+      .then((response) => response.json())
+       .then((data) => setTicketsList(data));
+  }, []);
 
+  return (
+    <div className="Body_Big_Container">
+      {ticketsList.map((ticket) => (
+      <div key={ticket.id} className="Body_Big_Container_box">{ticket.tema}</div>
+      ))}
+    </div>
+  );
 }
 
 export default Body;
